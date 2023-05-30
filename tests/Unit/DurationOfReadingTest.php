6af9279c710143b79aa131f2927a0261fb4b69cb
AzadGh95 <a.ghassemi92@gmail.com>
2023-05-30 13:05:56 +0330

    Unit test

---
+++ tests/Unit/DurationOfReadingTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Helpers\DurationOfReading;
+use PHPUnit\Framework\TestCase;
+
+class DurationOfReadingTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testCanGetDurationOfReadingText()
+    {
+        // 1 second per word
+        $text = 'this is for test';
+
+        $dor = new DurationOfReading($text);
+
+        $this->assertEquals(4, $dor->getTimePerSecond());
+        $this->assertEquals(4 / 60, $dor->getTimePerMinute());
+    }
+}
